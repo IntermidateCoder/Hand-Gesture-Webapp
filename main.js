b026c3f889db393e491b197acedc9c7c2635e61a
IntermidateCoder <88549450+IntermidateCoder@users.noreply.github.com>
2022-12-13 17:21:27 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+// https://teachablemachine.withgoogle.com/models/2cBB4odo5/
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    })
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2cBB4odo5/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        if (results[0].label == "Thumbsup") {
+            document.getElementById("emoji1").innerHTML = "&#128077;"
+        }
+        if (results[0].label == "Thumbsdown") {
+            document.getElementById("emoji1").innerHTML = "&#128078;"
+        }
+        if (results[0].label == "Amazing") {
+            document.getElementById("emoji1").innerHTML = "&#128076;"
+        }
+        if (results[0].label == "Cross") {
+            document.getElementById("emoji1").innerHTML = "&#129310;"
+        }
+
+        if (results[0].label == "tick mark") {
+            document.getElementById("emoji1").innerHTML = "&#10003;"
+        }
+    }
+}
